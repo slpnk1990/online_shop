73a66b2ac4292703c2dc746f93dbd8aba1420a81
slpnk1990 <dimastroi@tut.by>
2022-04-23 12:46:46 +0300

    main

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import DeleteView
+
+from .models import Notebook, Smartphone
 
 
 def test_view(request):
     return render(request, 'base.html', {})
+
+
+class ProductDetailView(DeleteView):
+    
+    CT_MODEL_MODEL_CLASS = {
+        'notebook': Notebook,
+        'smartphone': Smartphone
+    }
+    
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
