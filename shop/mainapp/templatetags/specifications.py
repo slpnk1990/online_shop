ea09fbc9d9b5f3dd04c4af5cefa2db295e1c57b3
slpnk1990 <dimastroi@tut.by>
2022-04-25 22:57:40 +0300

    main

--- shop/mainapp/templatetags/specifications.py
+++ shop/mainapp/templatetags/specifications.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.safestring import mark_safe
 
+from mainapp.models import Smartphone
+
 register = template.Library()
 
 
@@ -57,4 +59,9 @@ def get_product_spec(product, model_name):
 @register.filter
 def product_spec(product):
     model_name = product.__class__._meta.model_name
+    if isinstance(product, Smartphone):
+        if not product.sd:
+            PRODUCT_SPEC['smartphone'].pop('Максимальный объем встраиваемой памяти')
+        else:
+            PRODUCT_SPEC['smartphone']['Максимальный объем встраиваемой памяти'] = 'sd_volume_max'
     return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
