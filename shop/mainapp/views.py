646bd3ccd16196bceeac5716988880d2abdfc16d
slpnk1990 <dimastroi@tut.by>
2022-05-26 23:07:43 +0300

    main

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import DeleteView
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
+from django.views.generic import DeleteView, View
 
-from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
+
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
 
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+class BaseView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page('notebook', 'smartphone')
+        context = {
+            'categories': categories,
+            'products': products,
+            'cart': cart
+        }
+        return render(request, 'base.html', context)
 
 
-class ProductDetailView(DeleteView):
+class ProductDetailView(CategoryDetailMixin, DeleteView):
     
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -24,11 +37,42 @@ class ProductDetailView(DeleteView):
     context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
 
 
-class CategoryDetailView(DeleteView):
+class CategoryDetailView(CategoryDetailMixin, DeleteView):
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+    
+    
+class AddToCartView(View):
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
+    
+
+class CartView(View):
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
